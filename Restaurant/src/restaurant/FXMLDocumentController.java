0926ebf7502c31aa52710887fc40e9539746d503
mariamgaber123 <mariamgaberfarg@gmail.com>
2025-12-16 02:04:28 +0200

    about page

--- Restaurant/src/restaurant/FXMLDocumentController.java
+++ Restaurant/src/restaurant/FXMLDocumentController.java
@@ -9,27 +9,54 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
-
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 /**
  *
  * @author hp
  */
+
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    public Label label;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    public AnchorPane root;
+
+    @FXML
+    public ImageView backgroundImage;
+
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // ربط الخلفية بحجم الصفحة
+        backgroundImage.fitWidthProperty().bind(root.widthProperty());
+        backgroundImage.fitHeightProperty().bind(root.heightProperty());
+    }
+
+    @FXML
+    public void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+
+   @FXML
+public void gotomembers(ActionEvent event) throws Exception {
+            System.out.println("Team Members button clicked!");
+    Parent root = FXMLLoader.load(getClass().getResource("members.fxml"));
+    Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage.setScene(new Scene(root));
+    stage.setResizable(false);
+    stage.show();
+}
     
 }
--- Restaurant/src/restaurant/Restaurant.java
+++ Restaurant/src/restaurant/Restaurant.java
@@ -18,14 +18,15 @@ import javafx.stage.Stage;
 public class Restaurant extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
+public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("about.fxml"));
+    Scene scene = new Scene(root);
+    
+    stage.setScene(scene);
+    stage.setTitle("Restaurant");
+    stage.setResizable(false);
+    stage.show();
+}
 
     /**
      * @param args the command line arguments
